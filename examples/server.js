e1e2d09b8b5525824957db8c8bcc7b18378ee863
Merlin-Liu <is_liuguangfu@163.com>
2019-06-14 09:07:28 +0800

    test(emample): 错误处理demo
    编写错误处理测试代码

--- examples/server.js
+++ examples/server.js
@@ -62,6 +62,7 @@ router.get('/simple/get', function (req, res) {
     msg: `hello world`
   })
 })
+
 router.get('/base/get', function (req, res) {
   res.json(req.query)
 })
@@ -69,16 +70,6 @@ router.post('/base/post', function (req, res) {
   res.json(req.body)
 })
 router.post('/base/buffer', function (req, res) {
-  // let msg = []
-  // req.on('data', (chunk) => {
-  //   if (chunk) {
-  //     msg.push(chunk)
-  //   }
-  // })
-  // req.on('end', () => {
-  //   let buf = Buffer.concat(msg)
-  //   res.json(buf.toJSON())
-  // })
   let data = {}
   req.on('data', (chunk) => {
     if (chunk) {
@@ -89,6 +80,25 @@ router.post('/base/buffer', function (req, res) {
   req.on('end', () => {
     res.json(data.toString())
   })
+})
+
+router.get('/error/get', function (req, res) {
+  if (Math.random() > 0.5) {
+    res.json({
+      msg: `hello world`
+    })
+  } else {
+    res.status(500)
+    res.end()
+  }
+})
+
+router.get('/error/timeout', function (req, res) {
+  setTimeout(() => {
+    res.json({
+      msg: `hello world`
+    })
+  }, 3000)
 })
 
 app.use(router)
